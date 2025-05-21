41cfd4f2423b5a3b6c2d21dc1949b7a49c767f93
tjsos <tony.jacob@uri.edu>
2025-05-20 20:17:29 -0400

    ros2 version

---
+++ launch/bringup_simulation.launch.py
@@ -0,0 +1,39 @@
+import os
+import yaml
+import pathlib
+from launch import LaunchDescription
+import launch.actions
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch.substitutions import EnvironmentVariable
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+
+    #ALPHA_RISE
+    alpha_rise_bringup_launch_file = os.path.join(get_package_share_directory('alpha_rise_bringup'), 
+                                                  'launch', 'bringup_simulation.launch.py')
+    alpha_rise_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(alpha_rise_bringup_launch_file)
+    )
+
+    #MSIS_PCL
+    msis_pcl_launch_file = os.path.join(get_package_share_directory('msis_pcl'), 
+                                                  'launch', 'msis_pcl.launch.py')
+    msis_pcl_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(msis_pcl_launch_file)
+    )
+
+    pcl_filter_launch_file = os.path.join(get_package_share_directory('pcl_proc'), 
+                                                  'launch', 'filter.launch.py')
+    pcl_filter_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(pcl_filter_launch_file)
+    )
+
+    return LaunchDescription([
+        alpha_rise_bringup,
+        msis_pcl_launch,
+        pcl_filter_launch
+    ])

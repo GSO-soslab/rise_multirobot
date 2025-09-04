aa31abd9a8d9a6eaf51b3e303b46161b414f706f
mzhouURI <mzhou@uri.edu>
2025-09-04 11:20:58 -0400

    added wamv sim

--- launch/bringup_multi_simulation.launch.py
+++ launch/bringup_multi_simulation.launch.py
@@ -19,9 +19,29 @@ def generate_launch_description():
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("rise_multirobot"), 'launch','include','simulation.launch.py')]),
     )
 
+    wamv_sim_drivers = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("wamv_rise_bringup"), 'launch','include','simulation_drivers.launch.py')]),
+    )
 
+    wamv_localization = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("wamv_rise_bringup"), 'launch','bringup_pi_localization.launch.py')]),
+    )
+
+    wamv_mvp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("wamv_rise_bringup"), 'launch','bringup_pi_mvp.launch.py')]),
+    )
+
+    foxglove = IncludeLaunchDescription(
+    XMLLaunchDescriptionSource(
+        os.path.join(
+            get_package_share_directory('foxglove_bridge'),
+            'launch/foxglove_bridge_launch.xml')),
+    )
 
     return LaunchDescription([
+        foxglove,
         simulation,
- 
+        wamv_sim_drivers,
+        wamv_localization,
+        wamv_mvp
     ])
